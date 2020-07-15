0ebe927a23e6814830bb767f8268aede4cd5c908
meatyhippo <40859405+meatyhippo@users.noreply.github.com>
2020-07-15 17:43:39 +0200

    Create retail_JSON_Page.js
    had this one for a while.
    - hit the bookmark
    - fill in the prompts
    - json opens in a new page

---
+++ QoL/Bookmarklets/retail_JSON_Page.js
@@ -0,0 +1,23 @@
+!function(){
+    let domain = window.location.host;
+    let RAD = document.querySelector('#help_account_id > var').innerHTML;
+    let settings = {
+        APIendpoint : window.prompt('Which api endpoint?(case sensitive)','Item'),
+        AddRelation : window.prompt('Any relations to load? Or leave empty',''),
+        ID : window.prompt('Specific id? (found in the url of the item)',''),
+    };
+    settings.AddRelation = settings.AddRelation.replace(',','","');
+    let query = "";
+    if (settings.ID.length > 0 && settings.AddRelation.length > 0){
+        query+=`/${settings.ID}.json?load_relations=["${settings.AddRelation}"]`;
+    } else if (settings.AddRelation.length > 0){
+        query+=`.json?load_relations=["${settings.AddRelation}"]`;
+    } else if (settings.ID.length > 0){
+        query+=`/${settings.ID}.json`;
+    } else {
+        query+='.json';
+    }
+    let baseurl = `https://${domain}/API/Account/${RAD}/${settings.APIendpoint}${query}`;
+    console.log(baseurl);
+    window.open(encodeURI(baseurl));
+}();
