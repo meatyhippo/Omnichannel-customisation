a022551f22ac18368b853e8cac2ed9acce11d7b0
rubenvanhee <37211963+rubenvanhee@users.noreply.github.com>
2021-10-04 10:40:38 +0200

    commit to master

--- Scripts/eCom/shop_info/partneroffice.js
+++ Scripts/eCom/shop_info/partneroffice.js
@@ -1,6 +1,7 @@
 (()=>{
-	if (location.pathname == '/partners/apps/app/statistics')
-	$.getScript(`https://cdn.jsdelivr.net/gh/meatyhippo/Omnichannel-customisation@${version}/Scripts/eCom/Partners/export_app.js`, (script, textStatus, jqXHR)=>{
-		/**/console.log(script, textStatus, jqXHR);
-	});
-})
+	if (location.pathname == '/partners/apps/app/statistics'){
+		$.getScript(`https://cdn.jsdelivr.net/gh/meatyhippo/Omnichannel-customisation@${version}/Scripts/eCom/Partners/export_app.js`, (script, textStatus, jqXHR)=>{
+			/**/console.log(script, textStatus, jqXHR);
+		});
+	}
+})();
